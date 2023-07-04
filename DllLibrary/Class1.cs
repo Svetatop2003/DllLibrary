de9003e3757725458f9f1b4c868a29810073acbf
316de <316de@315-DE-4>
2023-07-04 10:23:58 +0300

    Добавьте файлы проекта.

---
+++ DllLibrary/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllLibrary
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Product> Products = new List<Product>();
+
+        public Store(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine("Инфо о складе:\n" +
+                $"\tНазвание: {Name}\n" +
+                $"\t:Адрес {Address}\n\n" + "Список товаров:");
+
+            Products.Sort();
+            foreach (Product product in Products)
+            {
+                product.Show();
+            }
+        }
+    }
+    public class Product : IComparable<Product>
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public Product(string name, string number)
+        {
+            Name = name;
+            Number = number;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Информация о складе:\n" + $"\tНазвание: {Name}" + $"Количество\t: {Number}");
+        }
+        public int CompareTo(Product other)
+        {
+            return (this.Name + this.Number).CompareTo(other.Name + other.Number);
+        }
+    }
+}
+
+    
+
+    
+
+
+
+
+
+
+
+
+
---
+++ Main_Stahanova/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using DllLibrary;
+
+namespace Main_Stahanova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Store store = new Store("Нежный аллигатор", "Москва");
+            for (int i = 1; i <= 3; i++)
+            {
+               store.Products.Add(new Product($"Инструменты. Набор {i}", "17"));
+               store.Products.Add(new Product($"Игрушки. Набор {i}", "13"));
+            }
+            store.ShowAll();
+        }
+    }
+}
